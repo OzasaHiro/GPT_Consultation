0a2cc846595c25657658c1c761a3bb774fcb6b1a
Jeron Wong <ThisIsJeron@users.noreply.github.com>
2023-06-04 13:25:55 -0700

    added iframe into index.js

--- consult-dashboard/nerdlets/consult-dashboard/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-// https://docs.newrelic.com/docs/new-relic-programmable-platform-introduction
-
-export default class ConsultDashboardNerdlet extends React.Component {
-  render() {
-    return <h1>Hello, consult-dashboard Nerdlet!</h1>;
-  }
-}
