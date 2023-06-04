01426b5e75a06414207cc01bceb021367d9e82a1
Jeron Wong <ThisIsJeron@users.noreply.github.com>
2023-06-04 14:59:06 -0700

    add working nerdlet

---
+++ nerdlet_works/nerdlets/nr1-howto-add-time-picker-nerdlet/index.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import {
+  Grid,
+  GridItem,
+  HeadingText,
+  AreaChart,
+  TableChart,
+  PieChart
+} from 'nr1';
+
+// https://docs.newrelic.com/docs/new-relic-programmable-platform-introduction
+
+export default class Nr1HowtoAddTimePicker extends React.Component {
+    constructor(props){
+        super(props)
+        this.accountId = 3963456;
+    }
+    render() {
+        const avgResTime = `SELECT average(duration) FROM Transaction FACET appName TIMESERIES AUTO`;
+        const trxOverview = `FROM Transaction SELECT count(*) as 'Transactions', apdex(duration) as 'apdex', percentile(duration, 99, 95) FACET appName`;
+        const errCount = `FROM TransactionError SELECT count(*) as 'Transaction Errors' FACET error.message`;
+        const responseCodes = `SELECT count(*) as 'Response Code' FROM Transaction FACET httpResponseCode`;
+        return (
+            <>
+            <Grid
+            className="primary-grid"
+            spacingType={[Grid.SPACING_TYPE.NONE, Grid.SPACING_TYPE.NONE]}
+            >
+            
+            <GridItem className="primary-content-container" columnSpan={6}>
+                <main className="primary-content full-height">
+                <HeadingText spacingType={[HeadingText.SPACING_TYPE.MEDIUM]} type={HeadingText.TYPE.HEADING_4}>
+                    Transaction Overview
+                </HeadingText>
+                <TableChart fullWidth accountId={this.accountId} query={avgResTime} />
+                </main>
+            </GridItem>
+            
+            <GridItem className="primary-content-container" columnSpan={6}>
+                <main className="primary-content full-height">
+                <HeadingText spacingType={[HeadingText.SPACING_TYPE.MEDIUM]} type={HeadingText.TYPE.HEADING_4}>
+                    Average Response Time
+                </HeadingText>
+                <AreaChart fullWidth accountId={this.accountId} query={avgResTime} />
+                </main>
+            </GridItem>
+            
+            
+            <GridItem className="primary-content-container" columnSpan={6}>
+                <main className="primary-content full-height">
+                <HeadingText spacingType={[HeadingText.SPACING_TYPE.MEDIUM]} type={HeadingText.TYPE.HEADING_4}>
+                    Generative Agent
+                </HeadingText>
+                <iframe src="https://ozasahiro-gpt-consultation-chat-app-x42yr3.streamlit.app/?embedded=true" width="650" height="300" />
+                </main>
+            </GridItem>
+
+            <GridItem className="primary-content-container" columnSpan={6}>
+                <main className="primary-content full-height">
+                <HeadingText spacingType={[HeadingText.SPACING_TYPE.MEDIUM]} type={HeadingText.TYPE.HEADING_4}>
+                    Response Code
+                </HeadingText>
+                <PieChart fullWidth accountId={this.accountId} query={responseCodes} />
+                </main>
+            </GridItem>
+        </Grid>
+        </>
+        );
+    }
+}
