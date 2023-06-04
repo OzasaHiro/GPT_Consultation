b728cdd74ebdc27ea885440968e1c5cbe1a9d3fa
Jeron Wong <ThisIsJeron@users.noreply.github.com>
2023-06-04 14:03:16 -0700

    working nerdpack?

---
+++ my-awesome-nerdpack/nerdlets/test/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// https://docs.newrelic.com/docs/new-relic-programmable-platform-introduction
+
+export default class TestNerdlet extends React.Component {
+  render() {
+    return <iframe src="https://ozasahiro-gpt-consultation-chat-app-x42yr3.streamlit.app/?embedded=true" width="1366" height="768"/>;
+  }
+}
